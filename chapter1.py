b6571d0b3fb5fc897f9bb11691c8caae086189d0
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-05-18 12:50:43 +0600

    up

--- chapter1.py
+++ chapter1.py
@@ -4,10 +4,10 @@ img = cv.imread("C:\\Users\\imran.hossain\\Pictures\\Camera Roll\\fahad.jpg")
 cv.imshow("Image",img)
 cv.waitKey(0)
 
-# vdo = cv.VideoCapture("C:\\Users\\imran.hossain\\Pictures\\New folder\\video.mp4")
-#
-# while True:
-#     success, img = vdo.read()
-#     cv.imshow("video", img)
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
+vdo = cv.VideoCapture("C:\\Users\\imran.hossain\\Pictures\\New folder\\video.mp4")
+
+while True:
+    success, img = vdo.read()
+    cv.imshow("video", img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
