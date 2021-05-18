9324a164f0bebd7974ef3e3c0c0aed65019af185
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-05-18 12:56:04 +0600

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 # print("okk")
-# img = cv.imread("C:\\Users\\imran.hossain\\Pictures\\Camera Roll\\fahad.jpg")
-# cv.imshow("Image",img)
-# cv.waitKey(0)
+img = cv.imread("C:\\Users\\imran.hossain\\Pictures\\Camera Roll\\fahad.jpg")
+cv.imshow("Image",img)
+cv.waitKey(0)
 
 vdo = cv.VideoCapture("C:\\Users\\imran.hossain\\Pictures\\New folder\\video.mp4")
 
-while True:
-    success, img = vdo.read()
-    cv.imshow("video", img)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+# while True:
+#     success, img = vdo.read()
+#     cv.imshow("video", img)
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#         break
