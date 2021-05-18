7bd3f6de0dbad38a527df822b0bd4d1f45e3ba49
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-05-18 12:59:21 +0600

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 # print("okk")
 img = cv.imread("C:\\Users\\imran.hossain\\Pictures\\Camera Roll\\fahad.jpg")
+
+imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Image",img)
+cv.imshow(" Gray Image",imgGray)
 cv.waitKey(0)
 
 # vdo = cv.VideoCapture("C:\\Users\\imran.hossain\\Pictures\\New folder\\video.mp4")
