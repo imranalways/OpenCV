7e097f1f2ff02e6460163aa9943701193e24e190
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-05-18 13:02:14 +0600

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -3,8 +3,13 @@ import cv2 as cv
 img = cv.imread("C:\\Users\\imran.hossain\\Pictures\\Camera Roll\\fahad.jpg")
 
 imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+imgBlur = cv.GaussianBlur(img,(7,7),0)
+
 cv.imshow("Image",img)
 cv.imshow(" Gray Image",imgGray)
+cv.imshow(" Blur Image",imgBlur)
+
 cv.waitKey(0)
 
 # vdo = cv.VideoCapture("C:\\Users\\imran.hossain\\Pictures\\New folder\\video.mp4")
